7e4b635b26aaad8d8c0cfd99edd19e689924e7ef
HoneyehYz <yazdizadehhoneyeh@yahoo.com>
2020-11-20 18:42:30 -0500

    initial front end

---
+++ notes-app-client/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Goal Tracker</h1>
+        <p className="text-muted">This is a goal tracker app. This is the Homepage</p>
+      </div>
+    </div>
+  );
+}
