a6e7af705d0f0138a858b8244c48f3ac1d2928ae
Anna Kaliza <annkaliza@gmail.com>
2020-11-23 09:17:51 -0500

    added auth APIs

--- express_backend/routes/users.js
+++ express_backend/routes/users.js
@@ -1,9 +1,60 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+
+  // Create a new user
+  router.post('/', (req, res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 12);
+    db.query(`INSERT INTO users(username, password, email) VALUES($1::string, $2::string, $3::string)` [req.username, password, req.email]).then(() => {
+      setTimeout(() => {
+        response.status(204).json({});
+      }, 1000);
+    })
+    .catch(error => console.log(error));
+  });
+
+  /**
+   * Check if a user exists with a given username and password
+   * @param {String} email
+   * @param {String} password encrypted
+   */
+  const login =  function(email, password) {
+    return getUserWithEmail(email)
+    .then(user => {
+      if (bcrypt.compareSync(password, user.password)) {
+        return user;
+      }
+      return null;
+    });
+  }
+  exports.login = login;
+
+  router.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    login(email, password)
+      .then(user => {
+        if (!user) {
+          res.send({error: "error"});
+          return;
+        }
+        req.session.userId = user.id;
+        res.send({user: {name: user.name, email: user.email, id: user.id}});
+      })
+      .catch(e => res.send(e));
+  });
+  
+  const getUserWithEmail = function(email) {
+    return db.query(`SELECT * FROM users
+    WHERE email = $1`, [email])
+    .then(res => res.rows[0])
+    .catch(rej => null);
+  }
+  exports.getUserWithEmail = getUserWithEmail;
+  router.post('/logout', (req, res) => {
+    req.session.userId = null;
+    res.send({});
+  });
 
 module.exports = router;
