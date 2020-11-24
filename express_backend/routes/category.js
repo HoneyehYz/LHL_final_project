531e5e24c3ab0e5dd3bc905912539f32041a52e0
Anna Kaliza <annkaliza@gmail.com>
2020-11-24 05:25:31 -0500

    added catogories API

---
+++ express_backend/routes/category.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+
+module.exports = db => {
+  router.get("/categories", (request, response) => {
+    db.query(
+      `
+      SELECT
+        id,
+        name
+      FROM categories
+    `
+    ).then(({ rows: categories}) => {
+      response.json(categories);
+    });
+  });
+  router.post("/categories", (request, response) => {
+    db.query(
+      `
+    INSERT categories(
+        name
+        VALUES($1::string)`[
+          (req.name)
+        ]
+    ).th   .then(() => {
+      setTimeout(() => {
+        response.status(204).json({});
+      }, 1000);
+    })
+    .catch((error) => console.log(error));
+  });
+
+
+
+  return router;
+};
