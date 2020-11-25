1e9d9341025498580484fd910b3020752db011d6
HoneyehYz <yazdizadehhoneyeh@yahoo.com>
2020-11-24 19:32:25 -0500

    Chat and Chart initial

--- notes-app-client/src/containers/Chat.js
+++ notes-app-client/src/containers/Chat.js
@@ -1,15 +1,85 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {Launcher} from 'react-chat-window'
 import "./Chat.css"
-
-
-export default function Chat() {
-  return (
-      <div className="inc-exp-container">
-      <div>
-     
-      <p className="money plus">Chat</p>
-      </div>
-    
-      </div>
-  )
+class Chat extends Component {
+ 
+  constructor() {
+    super();
+    this.state = {
+      messageList: []
+    };
+  }
+ 
+  _onMessageWasSent(message) {
+    this.setState({
+      messageList: [...this.state.messageList, message]
+    })
+    this._sendMessage("Hey")
+  }
+ 
+  _sendMessage(text) {
+    if (text.length > 0) {
+      this.setState({
+        messageList: [...this.state.messageList, {
+          author: 'them',
+          type: 'text',
+          data: { text }
+        }]
+      })
+    }
+  }
+ 
+  render() {
+    return (
+  
+    <div>
+      <p className="par"> Here at Goal Tracker we want to make sure you have the best support. So if you need any help
+        with the Goal Tracker application we are  here to support you. Just click on the Chat icon and wait for the
+        next representative. 
+      </p>
+      <Launcher
+        agentProfile={{
+          teamName: 'Goal Tracker Support',
+          imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
+        }}
+        onMessageWasSent={this._onMessageWasSent.bind(this)}
+        messageList={this.state.messageList}
+        showEmoji
+      />
+    </div>)
+  }
 }
+
+export default Chat; 
+
+
+
+
+
+// import React, { Component } from 'react';
+// import { Chat, addResponseMessage, addLinkSnippet, addUserMessage } from 'react-chat-popup';
+  
+// class Chats extends Component {
+//   componentDidMount() {
+//     addResponseMessage("Hello How can I help you today?");
+//   }
+ 
+//   handleNewUserMessage = (newMessage) => {
+//     console.log(`New message incoming! ${newMessage}`);
+//     // Now send the message throught the backend API
+//   }
+ 
+//   render() {
+//     return (
+//       <div className="App">
+//         <Chat
+//           handleNewUserMessage={this.handleNewUserMessage}
+//           title="Goal Tracker Support"
+//           subtitle="And my cool subtitle"
+//         />
+//       </div>
+//     );
+//   }
+// }
+ 
+// export default Chats;
--- notes-app-client/src/containers/Dashboard.js
+++ notes-app-client/src/containers/Dashboard.js
@@ -1,25 +1,68 @@
-import React from 'react'
 import "./Dashboard.css"
-
-
-export default function Dashboard() {
-  
-  return (
+import React, {Component} from "react";
+import CanvasJSReact from './canvasjs.react';
+const CanvasJS = CanvasJSReact.CanvasJS;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+class Dashboard extends Component {
+  render(){
+    const options = {
+			animationEnabled: true,
+			exportEnabled: true,
+			theme: "light2", // "light1", "dark1", "dark2"
+			title:{
+				text: "Performance Chart"
+			},
+			axisY: {
+				title: "Score",
+				suffix: ""
+			},
+			axisX: {
+				title: "Week ",
+				prefix: "W",
+				interval: 1
+			},
+			data: [{
+				type: "line",
+				toolTipContent: "Week {x}: {y}",
+				dataPoints: [
+					{ x: 1, y: 0.5 },
+					{ x: 2, y: 1 },
+					{ x: 3, y: 0.5 },
+					{ x: 4, y: 0 },
+					{ x: 5, y: 0.5 },
+					{ x: 6, y: 1 },
+					{ x: 7, y: 1 }
+				]
+			}]
+		}
+    return (
     
-<div className="row">
-  <div className="column" style={{backgroundColor: "grey"}}>
-    <h3>Daily Performance</h3>
-    <p>Some text..</p>
-  </div>
-  <div className="column" style={{backgroundColor: "lightgrey"}}>
-    <h3>Upcoming Milestone</h3>
-    <p>Milestone 1</p>
-    <p>Milestone 2</p>
-  </div>
-  <div className="column" style={{backgroundColor: "grey"}}>
-    <h3>Performance</h3>
-    <p>Some text..</p>
-  </div>
-</div>
-  )
+      // <div className="row">
+      //   <div className="column" style={{backgroundColor: "grey"}}>
+      //     <h3>Daily Performance</h3>
+      //     <p>Some text..</p>
+      //   </div>
+      //   <div className="column" style={{backgroundColor: "lightgrey"}}>
+      //     <h3>Upcoming Milestone</h3>
+      //     <p>Milestone 1</p>
+      //     <p>Milestone 2</p>
+      //   </div>
+      //   <div className="column" style={{backgroundColor: "grey"}}>
+      //     <h3>Performance</h3>
+      //     <p>Some text..</p>
+      //   </div>
+      //   <script type="text/javascript" src="canvasjs.min.js"></script>
+        <div> 
+          <h2>Welcome to the Dashboard</h2>
+          <div style={{color:"red",backgroundColor:"yellow", width:"50%", height:"300px",display:"flex", flexDirection:"row"}}> A </div>
+        <div style={{color:"red",backgroundColor:"purple", width:"50%", height:"300px",display:"flex", flexDirection:"row"}}> A </div>
+        <CanvasJSChart options = {options} style={{padding:"80px"}} />
+
+        </div>
+        )
+
+  }
+
 }
+
+export default Dashboard; 
