2c6bc170307c0ccfa00e31726b3a6cbe3b8b3d64
evaEZ <eva.jingzhao@gmail.com>
2020-11-25 09:50:43 -0500

    update for goal

--- notes-app-client/src/containers/components/Goal/GoalListItem.js
+++ notes-app-client/src/containers/components/Goal/GoalListItem.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import "./GoalListItem.css"
-
-
 export default function GoalListItem(props) {
-  
+  console.log(props)
   return (
-    <li className="goal__card" onClick={()=>props.setGoalSelector(props.goal)}>
-      <h2 className="text--regular" value={props.goal}>{props.goal}</h2>
+    <li className="goal__card">
+      <h2 className="text--regular">{props.goal}</h2>
       <section>
         <h4>Deadline: {props.deadline}</h4>
       </section>
     
     <section>
       <section>
-        {/*}
         <img
           className="goal-button"
           src="/edit.png"
           alt="Edit"
-  />*/}
+          onClick={props.editGoal}
+        />
         <img
           className="goal-button"
           src="/trash.png"
