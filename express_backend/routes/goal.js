89653c3ab0ee54fe54a51d0871806010d43cbba1
Anna Kaliza <annkaliza@gmail.com>
2020-12-05 05:50:41 -0500

    Refactor the goal endpoint to use Async/Await

--- express_backend/routes/goal.js
+++ express_backend/routes/goal.js
@@ -2,33 +2,42 @@ const router = require("express").Router();
 
 module.exports = (db) => {
   // Endpoint for retriving goals
-  router.get("/goals", (request, response) => {
-    db.query(
-      `
-      SELECT
-      id, user_id, goal, deadline, milestone_id
-      FROM goals
-    `
-    )
-      .then(({ rows: goals }) => {
-        response.json({ message: "Goals retrieved successfully", goals });
-      })
-      .catch((error) => console.log(error));
+  router.get("/goals", async (req, res) => {
+    try {
+      const text =
+        "SELECT id, user_id, goal, deadline, milestone_id FROM goals";
+      const goals = await db.query(text);
+
+      return res
+        .status(200)
+        .json({ message: "Goals retrieved successfully", goals: goals.rows });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Could not retrieve goals" });
+    }
   });
 
   // Endpoit for creating a goal
-  router.post("/goals", (req, res) => {
-    const text = "INSERT INTO goals(user_id, goal, deadline, milestone_id) VALUES($1, $2, $3, $4) RETURNING *";
-    const values = [req.body.userId, req.body.goal, req.body.deadline, req.body.milestoneId];
+  router.post("/goals", async (req, res) => {
+    try {
+      const text =
+        "INSERT INTO goals(user_id, goal, deadline, milestone_id) VALUES($1, $2, $3, $4) RETURNING *";
+      const values = [
+        req.body.userId,
+        req.body.goal,
+        req.body.deadline,
+        req.body.milestoneId,
+      ];
 
-    db.query(text, values)
-      .then((data) => {
-        res.status(201).json({ message: "Goal created successfully", goal: data.rows[0] });
-      })
-      .catch((error) => { 
-        console.log(error)
-        res.status(500).json({ message: "Could not create the goal"});
-      });
+      const goal = await db.query(text, values);
+
+      res
+        .status(201)
+        .json({ message: "Goal created successfully", goal: goal.rows[0] });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Could not create the goal" });
+    }
   });
 
   return router;
