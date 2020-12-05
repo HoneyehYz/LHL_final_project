1cf7e75b8845cd66973d80889c9fe803f6136d64
Anna Kaliza <annkaliza@gmail.com>
2020-12-05 09:31:55 -0500

    Restructure the database from improvements

--- express_backend/db/schema/development.sql
+++ express_backend/db/schema/development.sql
@@ -5,6 +5,8 @@ VALUES
     ('Princess', 'peach@nintendo.com','test'),
     ( 'Daisy','daisy@nintendo.com','test'),
     ('Jack', 'donkey@nintendo.com', 'test');
+
+
 INSERT INTO categories (name) 
 VALUES
     ('Health'),
@@ -12,23 +14,16 @@ VALUES
     ('Family'),
     ('Sport'),
     ('Mental Health');
-INSERT INTO scores (score, date) 
-VALUES
-    ('0', '2020-11-24 11:00:00'),
-    ('0.5', '2020-11-25 11:00:00'),
-    ('1','2020-11-26 11:00:00'),
-    ('0.5','2020-11-27 11:00:00'),
-    ('0.5', '2020-11-30 11:00:00');
 
 
 INSERT INTO
-milestones(milestone, deadline, completed_at) 
+milestones(user_id, milestone, deadline, completed_at) 
 VALUES
-    ('Sketch ERD', '2020-11-25 11:00:00', '2020-11-24 11:00:00'),
-    ('Buy dry walls', '2020-11-27 11:00:00', '2020-11-24 11:00:00'),
-    ('Study until video 8', '2020-11-24 11:00:00', '2020-11-24 11:00:00'),
-    ('Read untill chapter 4', '2020-11-23 11:00:00', '2020-11-24 11:00:00'),
-    ('Dont eat at restuarant', '2020-11-20 11:00:00', '2020-11-24 11:00:00');
+    (1, 'Sketch ERD', '2020-11-25 11:00:00', '2020-11-24 11:00:00'),
+    (2, 'Buy dry walls', '2020-11-27 11:00:00', '2020-11-24 11:00:00'),
+    (3, 'Study until video 8', '2020-11-24 11:00:00', '2020-11-24 11:00:00'),
+    (1, 'Read untill chapter 4', '2020-11-23 11:00:00', '2020-11-24 11:00:00'),
+    (2, 'Dont eat at restuarant', '2020-11-20 11:00:00', '2020-11-24 11:00:00');
 
 
 INSERT INTO
@@ -48,6 +43,7 @@ VALUES
     ),
     ('5', 'Save 200$', '2020-12-24 11:00:00' ,'5');
 
+
 INSERT INTO
 messages(message, date, fromUser, toUser)
 VALUES
@@ -58,13 +54,13 @@ VALUES
     ('Hi, I have some technical difficulties add my milestone', '2020-11-18 11:00:00', '1', '2');
 
 INSERT INTO
-items(item, user_id, score_id, category_id) 
+tasks(task, user_id, score, completed, score_date) 
 VALUES
-    ('Flossing', '1', '1','1'),
-    ('Eat healthier', '2', '2','1'),
-    ('Read 10 page book everyday', '3', '3','2'),
-    ('Watch one episode of Friends', '4', '2','2'),
-    ('Go to sleep by 11', '3', '2','1');
+    ('Flossing', 1, null, false, null),
+    ('Eat healthier', 2, 1, true, '2020-12-19 11:00:00'),
+    ('Read 10 page book everyday', 3, null, false, null),
+    ('Watch one episode of Friends', 1, 2, true, '2020-12-21 13:00:00'),
+    ('Go to sleep by 11', 3, null, false, null);
 
 
 
