1c586a7a9f9381319517334f99f547f2353ba42e
Anna Kaliza <annkaliza@gmail.com>
2020-12-06 10:03:14 -0500

    Consume the backend api for user login

--- notes-app-client/src/containers/Login.js
+++ notes-app-client/src/containers/Login.js
@@ -1,20 +1,16 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { toast } from "react-toastify";
+import axios from "axios";
+
 import "./Login.css";
 import { useAppContext } from "../libs/contextLib";
-import { useHistory } from "react-router-dom";
-
-const data = {
-  email : "honey@yahoo.com",
-  username : "honey",
-  password: "123"
-}
 
 export default function Login() {
   const history = useHistory();
   const { userHasAuthenticated } = useAppContext();
-
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -25,15 +21,26 @@ export default function Login() {
 
   async function handleSubmit(event) {
     event.preventDefault();
-  
+
     try {
-      if(data.email && data.password){
+      if (email && password) {
+        const res = await axios.post(
+          "http://localhost:3005/api/v1/auth/login",
+          { email, password }
+        );
+
+        toast.success(res.data.message);
+        localStorage.setItem("userId", res.data.user.id);
+        localStorage.setItem("username", res.data.user.username);
         userHasAuthenticated(true);
         history.push("/default");
       }
-      
-    } catch (e) {
-      alert(e.message);
+    } catch (err) {
+      if (err.response.data.message) {
+        toast.error(err.response.data.message);
+      } else {
+        toast.error("Could not log you in");
+      }
     }
   }
 
