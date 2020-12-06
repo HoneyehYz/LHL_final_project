444910d3fdd40e573d5de5882649c7eb88c7e73e
Anna Kaliza <annkaliza@gmail.com>
2020-12-06 10:00:58 -0500

    Setup the React Toastify for notifications and retrieve persisted user details from the local storage

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
+import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import "./App.css";
 import Routes from "./Routes";
-import Nav from "react-bootstrap/Nav";
-import { LinkContainer } from "react-router-bootstrap";
 import { AppContext } from "./libs/contextLib";
 import { useHistory } from "react-router-dom";
-
-
 
 function App() {
   const history = useHistory();
@@ -15,9 +16,12 @@ function App() {
 
   function handleLogout() {
     userHasAuthenticated(false);
+    localStorage.removeItem("userId");
+    localStorage.removeItem("username");
     history.push("/login");
+    window.location.reload();
   }
-  
+
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="warning" expand="md" className="mb-3">
@@ -29,26 +33,30 @@ function App() {
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Nav activeKey={window.location.pathname}>
-          {isAuthenticated ? (
-        <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-        ) : (
-         <>
-        <LinkContainer to="/signup">
-        <Nav.Link>Signup</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/login">
-        <Nav.Link>Login</Nav.Link>
-      </LinkContainer>
-    </>
-    )}
+            {localStorage.getItem("userId") ? (
+              <>
+                <Nav.Link>{localStorage.getItem("username")}</Nav.Link>
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <LinkContainer to="/signup">
+                  <Nav.Link>Signup</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
       <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
         <Routes />
       </AppContext.Provider>
+      <ToastContainer />
     </div>
-  );  
+  );
 }
 
 export default App;
